944b519111d9149f6d6dc73d4ae83150fc07bac2
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2023-10-16 16:36:03 -0300

    Correção de bug

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { update } from "./update";
 
 export function main() {
+  const form = document.querySelector<HTMLFormElement>("form")!;
   const input = document.querySelector<HTMLInputElement>("input")!;
   const reset =
     document.querySelector<HTMLButtonElement>("button[type=reset]")!;
   const output = document.querySelector<HTMLOutputElement>("output")!;
+
+  form.addEventListener("submit", (e) => e.preventDefault());
   reset.addEventListener("click", () => {
     input.value = "";
     input.focus();
